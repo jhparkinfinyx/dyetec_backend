689ccda43b1c8bc931aebee62e4cd5631ec760d2
jhparkinfinyx <jhpark.infinyx@gmail.com>
2021-12-15 19:38:27 +0900

    add

--- tasks_app.py
+++
@@ -1,6 +0,0 @@
-from tasks import add
-result = add.delay(30000, 1337)
-
-print(result.ready())
-print(result.get())
-print(result.ready())
--- wsgi.py
+++
@@ -1,5 +0,0 @@
-# from app import app
-from hello import app
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
